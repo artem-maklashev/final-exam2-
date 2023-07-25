77a58212df5c19cbd5930522b961ed84430eafdd
Маклашев Артем <maklashev_ak@outlook.com>
2023-07-26 00:08:48 +0400

    add some tasks to db

--- работа с БД.sql
+++ работа с БД.sql
@@ -81,7 +81,7 @@ VALUES
 	("Chewy", "2022-11-01", "");
 
 ## Вьючные животные
-DROP TABLE IF EXISTS ackanimals_type;
+DROP TABLE IF EXISTS packanimals_type;
 CREATE TABLE packanimals_type(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	animals_type_id INT UNSIGNED NOT NULL DEFAULT 2,
@@ -89,7 +89,7 @@ CREATE TABLE packanimals_type(
 	FOREIGN KEY (animals_type_id) REFERENCES animals_type(id)
 );
 
-INSERT INTO pets_type(packanimals_type) 
+INSERT INTO packanimals_type(packanimals_type) 
 VALUES
 	("horse"),
 	("camel"),
@@ -115,44 +115,59 @@ VALUES
 	("Spirit", "2021-10-25", "пррр");
 
 
-
-
-
-
-
-
-
-### продолжить от сюда
-DROP TABLE IF EXISTS dogs;
-CREATE TABLE dogs (
+DROP TABLE IF EXISTS camels;
+CREATE TABLE camels (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   
-  pets_type_id INT UNSIGNED NOT NULL DEFAULT 2,
+  packanimals_type_id INT UNSIGNED NOT NULL DEFAULT 2,
   name VARCHAR(50) NOT NULL,
   birthday DATE,
   command VARCHAR(120),
   
-  FOREIGN KEY (pets_type_id) REFERENCES pets_type(id)
+  FOREIGN KEY (packanimals_type_id) REFERENCES packanimals_type(id)
 );
 
-INSERT INTO dogs(name, birthday, command)
+INSERT INTO camels(name, birthday, command)
 VALUES
-	("Rex", "2021-03-08", "ко мне, лапу, сидеть, лежать"),
-	("Tomas", "2022-09-16", "ко мне, место, лежать");
+	("Marlboro", "2015-03-08", "встать"),
+	("Lucky", "2018-07-01", "вперед");
 
-DROP TABLE IF EXISTS humsters;
-CREATE TABLE humsters (
+DROP TABLE IF EXISTS donkeys;
+CREATE TABLE donkeys (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   
-  pets_type_id INT UNSIGNED NOT NULL DEFAULT 3,
+  packanimals_type_id INT UNSIGNED NOT NULL DEFAULT 3,
   name VARCHAR(50) NOT NULL,
   birthday DATE,
   command VARCHAR(120) ,  
-  FOREIGN KEY (pets_type_id) REFERENCES pets_type(id)
+  FOREIGN KEY (packanimals_type_id) REFERENCES packanimals_type(id)
 );
 
-INSERT INTO humsters(name, birthday, command)
+INSERT INTO donkeys(name, birthday, command)
 VALUES
-	("Popcorn", "2023-03-08", ""),
-	("Chewy", "2022-11-01", "");
+	("Eeyore", "2019-03-08", "Но"),
+	("Accer", "2021-12-31", "Стой");
 
+## Удаляем из базы данных верблюдов
+DROP TABLE camels;
+
+## Создаем таблицу молодые животные
+DROP TABLE IF EXISTS young_animals;
+CREATE TABLE young_animals (
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  age INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO young_animals (name, age)
+SELECT name, TIMESTAMPDIFF(MONTH, birthday, NOW()) AS Возраст_мес
+FROM (
+	SELECT name, birthday FROM cats 
+	UNION ALL
+	SELECT name, birthday FROM dogs 
+) AS animals
+WHERE birthday > DATE_SUB(NOW(), INTERVAL 3 YEAR) AND birthday < DATE_SUB(NOW(), INTERVAL 1 YEAR);
+
+	
+WHERE age > 1 AND age < 3;
